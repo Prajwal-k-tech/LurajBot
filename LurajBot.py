c60c4e04731851b5adf43049e5fa36a93ba48e26
Prajwal Kumar K <69136413+Prajwal-k-tech@users.noreply.github.com>
2021-05-10 12:59:31 +0530

    Update LurajBot.py

--- LurajBot.py
+++ LurajBot.py
@@ -9,8 +9,30 @@ client = commands.Bot(command_prefix=".l ")
 async def on_ready():
     print("Bot is ready")
 @client.command()
-async def ping(ctx):
-    await ctx.send("ponggggg!!!")
+async def server(ctx):
+    name = str(ctx.guild.name)
+    description = str(ctx.guild.description)
+
+    owner = str(ctx.guild.owner)
+    id = str(ctx.guild.id)
+    region = str(ctx.guild.region)
+    memberCount = str(ctx.guild.member_count)
+
+    icon = str(ctx.guild.icon_url)
+
+    embed = discord.Embed(
+        title=name + " Server Information",
+        description=description,
+        color=discord.Color.blue()
+    )
+    embed.set_thumbnail(url=icon)
+    embed.add_field(name="Owner", value=owner, inline=True)
+    embed.add_field(name="Server ID", value=id, inline=True)
+    embed.add_field(name="Region", value=region, inline=True)
+    embed.add_field(name="Member Count", value=memberCount, inline=True)
+
+    await ctx.send(embed=embed
+
 @client.command()
 async def hi(ctx):
     await ctx.send("Hi im Luraj")
@@ -23,6 +45,11 @@ async def joke(ctx):
 @client.command()
 async def meme(ctx):
     await ctx.send(embed=await pyrandmeme())
+@client.command()
+async def ping(ctx):
+    await ctx.channel.send(f'Pong! {round (bot.latency * 1000)}ms ')
+@client.command()
+
 
 
 client.run("ODQwODYwOTU4NDA1NTU4MzAy.YJeWyg.wqw9TUN6oIrVG6pFonKrjtFUp1U")
