0b552c056deecfe9a0e1593af2e1ef596625bba3
Prajwal Kumar K <69136413+Prajwal-k-tech@users.noreply.github.com>
2021-12-01 11:56:22 +0530

    Update LurajBot.py

--- LurajBot.py
+++ LurajBot.py
@@ -9,7 +9,7 @@ client = commands.Bot(command_prefix=".l ")
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game('Cool Math Games'))
+    await client.change_presence(activity=discord.Game('HAPPY BIRTHDAY TEJASWINI!'))
     print("Bot is ready")
    
 
